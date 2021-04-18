f3027253a19265b18dd9b3326a4dc82ed15c0f65
McCray <tubach75@hotmail.com>
2021-04-18 12:01:33 -0400

    commit tree 18ce50625ecd6be5550e93b84b567ab94ff959a6

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(categories_attributes)
+  def categories_attributes=(categories_attributes) #naming the has the same as the method is confusing.
     categories_attributes.values.each do |category|
       if !category[:name].empty?
         self.categories << Category.find_or_create_by(name: category[:name])
@@ -13,4 +13,18 @@ class Post < ActiveRecord::Base
     end
   end
 
+
+=begin
+  def categories_attributes=(attributes) #attributes is a hash with keys of the index number of attributes.
+    #the index number has a key of a hash that contains a key of the attribute name and a value of the info entered into the form.
+    attributes.values.each do |category|
+      #attributes.values will place those {attribute_name: "value"} hashes into an array. category will then be an individual has that each iterated over.
+      #after that, you can work on the individual hashes just as you normally would.
+      binding.pry
+      if !category[:name].empty?
+        self.categories << Category.find_or_create_by(name: category[:name])
+      end
+    end
+  end
+=end
 end
